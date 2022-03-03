271e964c5a22810046bcc35e41fdc2da2fe996ee
JacobPooleCV <jpoole@gusd.net>
2022-03-03 08:31:25 -0800

    Moby

---
+++ Python/999_Exercises/Moby Dick/baseNEW.py
@@ -0,0 +1,20 @@
+sentence = "whale hello there. Don't we all love whales? I absolutely love whales! whales are so huge!!!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+#with open('moby.txt') as f:
+#    for line in f:
+#        sentence = line.strip()
+#        ##YOUR CODE GOES HERE
+#
+#f.close()
